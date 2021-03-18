6935b13f9fde4d5a759765f046e082dff0cf5a8c
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2021-03-18 15:28:12 -0700

    Add functionality outside of gameplay (rematch, new game, new match)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const board = (() => {
-  const squares = [];
+  let squares = [];
 
   const lines = [
     [0, 1, 2],
@@ -13,8 +13,8 @@ const board = (() => {
   ];
 
   const move = (marker, position) => (squares[position] ||= marker);
-
   const getValue = (index) => squares[index];
+  const clear = () => squares = [];
 
   const findWin = () => {
     return lines.find((line) =>
@@ -24,75 +24,249 @@ const board = (() => {
     );
   };
 
-  const stalemate = () => {
+  const isStalemate = () => {
     for (let i = 0; i < 9; i++) if (!squares[i]) return false;
     return true;
   };
 
-  return { move, findWin, stalemate, getValue };
+  return { move, findWin, isStalemate, getValue, clear, squares };
 })();
 
 const gamePlay = (() => {
-  const status = document.querySelector('.status');
   let xToMove = true;
+  let player1, player2;
 
   const switchPlayer = () => (xToMove = !xToMove);
 
-  const updateStatus = (params) => {
-    const winningLine = board.findWin();
-    displayController.displayBoard();
-    if (winningLine) {
-      status.innerHTML = xToMove ? '<h3>X Wins!</h3>' : '<h3>O Wins!</h3>';
-      displayController.makeUnavailableAll();
-      displayController.displayWin(winningLine);
-    } else if (board.stalemate()) {
-      status.innerHTML = "It's a draw!";
-    } else {
-      switchPlayer();
-      displayController.makeUnavailable(params.lastMove);
-    }
-  };
-
-  const move = (e) => {
-    const { index } = e.target.dataset;
+  const move = (e, i) => {
+    const index = e ? e.target.dataset.index : i;
     const marker = xToMove ? 'X' : 'O';
     board.move(marker, index);
     updateStatus({ lastMove: index });
   };
 
-  return { move };
+  const updateStatus = (params) => {
+    displayController.displayBoard();
+    const winningLine = board.findWin();
+    if (winningLine) {
+      if (xToMove) {
+        player1.addPoint();
+        displayController.showResult('<h3>Player 1 Wins!</h3>');
+      } else {
+        player2.addPoint();
+        displayController.showResult('<h3>Player 2 Wins!</h3>');
+      }
+      userPanels.showScores(player1, player2);
+      displayController.disableAll();
+      displayController.reset();
+      displayController.displayWin(winningLine);
+    } else if (board.isStalemate()) {
+      displayController.showResult("<h3>It's a draw!<h3>");
+    } else {
+      switchPlayer();
+      displayController.disable(params.lastMove);
+    }
+  };
+
+  function newMatch(e) {
+    e.preventDefault();
+    const selection1 = this.querySelector('[name=player-one]').value;
+    switch (selection1) {
+      case 'Human':
+        player1 = playerFactory('Player 1');
+        break;
+      case 'Computer (Easy)':
+        player1 = computerEasy();
+        break;
+      case 'Computer (Medium)':
+        player1 = computerMedium();
+        break;
+      default:
+        player1 = computerHard();
+    }
+    const selection2 = this.querySelector('[name=player-two]').value;
+    switch (selection2) {
+      case 'Human':
+        player2 = playerFactory('Player 2');
+        break;
+      case 'Computer (Easy)':
+        player2 = computerEasy();
+        break;
+      case 'Computer (Medium)':
+        player2 = computerMedium();
+        break;
+      default:
+        player2 = computerHard();
+    }
+    newGame();
+  }
+
+  function newGame() {
+    userPanels.restart();
+    userPanels.showScores(player1, player2);
+    userPanels.gameDisplay();
+    displayController.enableAll();
+    displayController.hideResult();
+  }
+
+  return { move, newMatch, newGame };
 })();
 
 const displayController = (() => {
   const squares = document.querySelectorAll('.square');
-  squares.forEach((square) => square.addEventListener('click', gamePlay.move));
+  const result = document.querySelector('.result');
+  const resultText = result.querySelector('h3');
+  const rematch = result.querySelector('.rematch');
+
+  rematch.addEventListener('click', gamePlay.newGame);
 
   const displayBoard = () => {
     squares.forEach((square, i) => {
       const marker = board.getValue(i);
-      if (marker) {
-        square.innerHTML = `<img src='/images/${marker}.svg'>`;
-      }
+      square.innerHTML = marker ? `<img src='/images/${marker}.svg'>` : '';
     });
   };
 
-  const displayWin = (winningLine) =>
+  const displayWin = (winningLine) => {
     winningLine.forEach((index) =>
       squares[index].classList.add('winning-line')
     );
+  };
 
-  const makeUnavailable = (index) => {
+  function showResult(message) {
+    resultText.innerHTML = message;
+    result.style.width = '90%';
+    result.style.height = '90%';
+    rematch.style.display = 'block';
+  }
+
+  const hideResult = () => {
+    resultText.innerHTML = '';
+    result.style.width = '0';
+    result.style.height = '0';
+    rematch.style.display = 'none';
+  }
+
+  const enableAll = () => {
+    squares.forEach((square) => {
+      square.addEventListener('click', gamePlay.move);
+      square.classList.remove('unavailable');
+    });
+  };
+
+  const disable = (index) => {
     lastMove = squares[index];
     lastMove.removeEventListener('click', gamePlay.move);
     lastMove.classList.add('unavailable');
   };
 
-  const makeUnavailableAll = () => {
+  const disableAll = () => {
     squares.forEach((square) => {
       square.removeEventListener('click', gamePlay.move);
       square.classList.add('unavailable');
     });
   };
 
-  return { makeUnavailable, makeUnavailableAll, displayBoard, displayWin };
+  const reset = () => {
+    squares.forEach((square) => square.classList.remove('winning-line'));
+  }
+
+  return { enableAll, disable, disableAll, displayBoard, displayWin, showResult, hideResult, reset };
+})();
+
+const playerFactory = (name) => {
+  let points = 0;
+  const getPoints = () => points;
+  const addPoint = () => points++;
+
+  return { name, getPoints, addPoint };
+}
+
+const computerEasy = () => {
+  const prototype = playerFactory('Computer (Easy)');
+
+  const move = () => {
+    do {
+      var i = Math.floor(Math.random() * 9)
+
+    } while (board.getValue(i))
+    gamePlay.move(null, i)
+  }
+
+  return Object.assign({}, prototype, {move})
+}
+
+const computerMedium = () => {
+  const prototype = playerFactory('Computer (Medium)');
+
+  const move = () => {
+    do {
+      var i = Math.floor(Math.random() * 9)
+
+    } while (board.getValue(i))
+    gamePlay.move(null, i)
+  }
+
+  return Object.assign({}, prototype, {move})
+}
+
+const computerHard = () => {
+  const prototype = playerFactory('Computer (Hard)');
+
+  const move = () => {
+    do {
+      var i = Math.floor(Math.random() * 9)
+
+    } while (board.getValue(i))
+    gamePlay.move(null, i)
+  }
+
+  return Object.assign({}, prototype, {move})
+}
+
+const userPanels = (() => {
+  const gamePanel = document.querySelector('.game-panel');
+  const restartButton = document.querySelector('.restart');
+  const newMatchButton = document.querySelector('.new-match');
+
+  restartButton.addEventListener('click', restart);
+  newMatchButton.addEventListener('click', newMatch);
+
+  const setupForm = document.querySelector('.setup-panel');
+  setupForm.addEventListener('submit', gamePlay.newMatch);
+
+  function reset() {
+    displayController.reset();
+    board.clear();
+    displayController.displayBoard();
+  }
+
+  function restart() {
+    reset();
+    displayController.enableAll();
+    displayController.hideResult();
+  }
+
+  function newMatch() {
+    reset();
+    setupForm.classList.remove('hidden');
+    gamePanel.classList.add('hidden');
+    displayController.disableAll();
+    displayController.reset();
+  }
+
+  function showScores(player1, player2) {
+    const p1 = document.querySelector('.player1-score');
+    const p2 = document.querySelector('.player2-score');
+    p1.textContent = `${player1.name}: ${player1.getPoints()}`
+    p2.textContent = `${player2.name}: ${player2.getPoints()}`
+  }
+
+  function gameDisplay() {
+    setupForm.classList.add('hidden');
+    gamePanel.classList.remove('hidden');
+  }
+
+  return { showScores, gameDisplay, restart }
+
 })();
